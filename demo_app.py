ebfc879c3c6e1da8a6907537e88dfb17aeaa9d0b
mdadn10 <fcb.adnan10@gmail.com>
2024-06-17 20:25:31 +0300

    .

--- demo_app.py
+++ demo_app.py
@@ -1,1 +1,8 @@
-
+import cv2
+import streamlit as st
+import openpyxl
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+import pandas as pd
+import seaborn as sns  # Import Seaborn for KDE plots
